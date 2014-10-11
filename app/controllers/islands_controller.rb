b663f20697b1e625d6b0cb8c6cdafdcfa9ce8dca
Tomohiko Himura <eiel.hal@gmail.com>
2014-10-11 15:05:15 +0900

    ページを作成

---
+++ app/controllers/islands_controller.rb
@@ -0,0 +1,7 @@
+class IslandsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'spots/index'
+
+  get 'spots/show'
+
+  get 'islands/index'
+
+  get 'islands/show'
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
